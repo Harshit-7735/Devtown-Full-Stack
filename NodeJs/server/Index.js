82772b0b0e745bdb8479aee44c94bd69b147b167
Harshit-7735 <letcs202205@global.org.in>
2024-12-15 20:16:51 +0530

    express

--- NodeJs/server/Index.js
+++ NodeJs/server/Index.js
@@ -1,41 +1,43 @@
 const express = require("express");
 const fs = require("fs/promises");
+const { parse } = require("path");
 // console.log(express);
 
+// create an express server
+
 const PORT = 8080;
-// express instance
 const app = express();
 
+// create a route
 app.get("/", (req, res) => {
-  // console.log(req);
-  const { headers, method, url, path, query } = req;
+  // we can get headers, body, query params, path params
+  const { headers, body, query, params } = req;
   console.log({
     headers,
-    method,
-    url,
-    path,
+    body,
     query,
+    params,
   });
 
-  res.status(200).send("Server is Running");
-});
-// app.get('/todos',(req,res)=>{
-//     res.status(200).send('todos')
-// })
-
-app.get("/todos", async (req, res) => {
-  try {
-    const {count} = req.query
-    const todos = await fs.readFile("./db.json", "utf-8");
-    const parsedtodos= JSON.parse(todos)
-    console.log(todos);
-    res.status(200).json((parsedtodos.slice(0,count)));
-  } catch (error) {
-    console.log(error);
-    res.status(500).json(error);
-  }
+  res.send("Hello World");
 });
 
+
+// create a todo route
+app.get('/todo',async(req,res)=>{
+    try {
+        const todos = await fs.readFile('./db.json','utf-8');
+        // console.log(todos);
+        parsedTodos = JSON.parse(todos);
+        res.status(200).send(parsedTodos.slice(0,5));
+        
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Internal Server Error');
+        
+    }
+})
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });

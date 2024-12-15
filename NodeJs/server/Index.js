2bf00991485838687a95e75fddd58f372a54148e
Harshit-7735 <letcs202205@global.org.in>
2024-12-15 21:16:08 +0530

    Authentication with query token

--- NodeJs/server/Index.js
+++ NodeJs/server/Index.js
@@ -6,47 +6,43 @@ const PORT = 8080;
 // expres instance
 const app = express();
 
+// middleware
+/*
 const logger = async (req, res, next) => {
-  // do something
-  const { headers, method, url, path, query } = req;
+  const {headers, method, url,query,path} = req;
   console.log({
     headers,
     method,
     url,
-    path,
     query,
+    path,
   });
   next();
-};
-
-// validate the token and if token is invalid throw the error
-const isAuthorised = async (req,res,next) => {
-  try {
-    const { token } = req.query;
-    if (!token) {
-      return res.status(401).json({ message: "Token is required" });
-    }
-    const users = await fs.readFile("./users.json", "utf-8");
-    const parsedUsers = JSON.parse(users);
-    const user = parsedUsers.find((user) => user.token === token)
-    if (!user) {
-      return res.status(401).json({ message: "invalid token" });
-    }
-    req.user = user;
-    next()
-  } catch (error) {
-    return res.status(500).json({ message: "something went wrong" });
-  }
-};
-app.use(express.json());
-// app.use(logger);
-
-app.get("/", (req, res) => {
-  // console.log(req)
-
-  res.status(200).send("SErver is running");
+}
+app.use(logger);  
+app.get('/',logger, (req, res) => {
+  res.send("Hello World");
 });
+*/
 
+/*
+app.post('/signup', async (req, res) => {
+  try {
+    const {email,password} = req.body;
+    return res.status(200).json({
+      email,
+      password,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+});
+*/
+
+// Authentification
+app.use(express.json());
 app.post("/signup", async (req, res) => {
   try {
     const { email, password } = req.body;
@@ -56,35 +52,64 @@ app.post("/signup", async (req, res) => {
       password,
       token,
     };
+
+    // read file
     const users = await fs.readFile("./users.json", "utf-8");
     const parsedUsers = JSON.parse(users);
     if (parsedUsers.find((user) => user.email === email)) {
-      return res
-        .status(409)
-        .json({ message: "User with such email already exists" });
+      return res.status(400).json({
+        message: "User already exists",
+      });
     }
+    // to add user to the file
     parsedUsers.push(user);
+
     await fs.writeFile("./users.json", JSON.stringify(parsedUsers));
-    return res.status(200).json({ token });
+    return res.status(200).json({
+      email,
+      password,
+    });
   } catch (error) {
     console.log(error);
-    res.status(500).json(error);
+    return res.status(500).json({
+      message: error.message,
+    });
   }
 });
 
-app.get("/todos",isAuthorised, async (req, res) => {
+// validate the token and if token is invalid throw the error
+const isAuthorised = async (req, res, next) => {
   try {
-    const { count = 10 } = req.query;
+    const { token } = req.query;
+    if (!token) {
+      return res.status(401).json({ message: "Token is required" });
+    }
+    const users = await fs.readFile("./users.json", "utf-8");
+    const parsedUsers = JSON.parse(users);
+    const user = parsedUsers.find((user) => user.token === token);
+    if (!user) {
+      return res.status(401).json({ message: "invalid token" });
+    }
+    req.user = user;
+    next();
+  } catch (error) {
+    return res.status(500).json({ message: "something went wrong" });
+  }
+};
 
+// for todos
+app.get("/todos", isAuthorised, async (req, res) => {
+  try {
     const todos = await fs.readFile("./db.json", "utf-8");
-    const parsedTodos = JSON.parse(todos);
-    res.status(200).json(parsedTodos.slice(0, count));
+    const parsesdTodos = JSON.parse(todos);
+    res.status(200).json(parsesdTodos);
   } catch (error) {
     console.log(error);
-    res.status(500).json(error);
+    return res.status(500).json({
+      message: error.message,
+    });
   }
 });
-
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });

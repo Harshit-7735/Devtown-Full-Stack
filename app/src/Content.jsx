8b76fcaa1fedd0bd42227d90becc4dee2e853457
Harshit-7735 <letcs202205@global.org.in>
2025-01-06 12:16:47 +0530

    state

--- app/src/Content.jsx
+++ app/src/Content.jsx
@@ -35,36 +35,38 @@ const Content = () => {
 
   // CREATION OF A TODO LIST
 
-  const [todos, setTodos] = useState([ ]);
+  const [todos, setTodos] = useState([]);
   const [input, setInput] = useState(""); // this will be used to store the value of the input field
 
-  const handleChange = (e) => { // this function will be used to handle the change event of the input field
+  const handleChange = (e) => {
+    // this function will be used to handle the change event of the input field
     setInput(e.target.value); // this will set the value of the input field to the value that the user types
   };
 
-  const addTodo = (e) => 
-  {
+  const addTodo = (e) => {
     setTodos([...todos, input]); // this will add the value of the input field to the todos array
     setInput(""); // this will clear the input field after the value has been added to the todos array
-  }
+  };
 
-const deleteTodo = (index) => {
-  const newTodos = [...todos];
-  newTodos.splice(index, 1);
-  setTodos(newTodos);
-}
+  const deleteTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
 
-return (
+  return (
     <div>
-     <h4>{todos.length > 0 ? "No Todos" : "Add Todos"}</h4>
-     <input value={input} onChange={handleChange} type="text" /> 
+      <h4>{todos.length > 0 ? "No Todos" : "Add Todos"}</h4>
+      <input value={input} onChange={handleChange} type="text" />
       <button onClick={addTodo}>Add Todo</button>
       <div>
         {todos.map((todo, index) => (
-          <li >{todo} <button onClick={deleteTodo}>Delete</button></li>
+          <li>
+            {todo} <button onClick={deleteTodo}>Delete</button>
+          </li>
         ))}
       </div>
-      </div>
-  )
+    </div>
+  );
 };
 export default Content;

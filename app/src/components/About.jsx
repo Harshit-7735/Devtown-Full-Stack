9b7a1e991b67c13a55000fcc8a1e6aa45894192d
Harshit-7735 <letcs202205@global.org.in>
2025-02-08 13:41:02 +0530

    lifecycle ...mount and unmount

--- app/src/components/About.jsx
+++ app/src/components/About.jsx
@@ -1,11 +1,28 @@
+import { useEffect, useState } from 'react';
 import{Link} from 'react-router-dom';
 const About = ({setRoute}) => {
+    const [count,setCount] = useState(0);
+    console.log("render");
+    useEffect(()=>{
+        console.log("mount");
+        return ()=>{
+            console.log("unmount");
+        }
+    },[]); // empty array means it will only run on mount and unmount 
+    useEffect(()=>{
+        console.log("count changed");
+    },[count]);
+    
     return ( 
         <>
-        <h1>About</h1>
+        <nav>
         <Link to="/">Home</Link>
         <br />
         <Link to="/contact">Contact</Link>
+        </nav>
+        <h1>About</h1>
+        {count}
+        <button onClick={()=>setCount(count+1)}>Increment</button>
         </>
      );
 }

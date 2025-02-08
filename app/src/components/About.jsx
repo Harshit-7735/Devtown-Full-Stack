46fb7ad8503b54ec60112e7a00514dfec517796a
Harshit-7735 <letcs202205@global.org.in>
2025-02-08 14:05:23 +0530

    Set Timer

--- app/src/components/About.jsx
+++ app/src/components/About.jsx
@@ -1,17 +1,28 @@
 import { useEffect, useState } from 'react';
 import{Link} from 'react-router-dom';
 const About = ({setRoute}) => {
-    const [count,setCount] = useState(0);
-    console.log("render");
+    // const [count,setCount] = useState(0);
+    // console.log("render");
+    // useEffect(()=>{
+    //     console.log("mount");
+    //     return ()=>{
+    //         console.log("unmount");
+    //     }
+    // },[]); // empty array means it will only run on mount and unmount 
+    // useEffect(()=>{
+    //     console.log("count changed");
+    // },[count]);
+
+    const [timer,setTimer] = useState(0);
+
     useEffect(()=>{
-        console.log("mount");
+        const interval = setInterval(()=>{
+            setTimer(timer+1);
+        },1000);
         return ()=>{
-            console.log("unmount");
+            clearInterval(interval);
         }
-    },[]); // empty array means it will only run on mount and unmount 
-    useEffect(()=>{
-        console.log("count changed");
-    },[count]);
+    },[timer]);
     
     return ( 
         <>
@@ -21,8 +32,11 @@ const About = ({setRoute}) => {
         <Link to="/contact">Contact</Link>
         </nav>
         <h1>About</h1>
-        {count}
-        <button onClick={()=>setCount(count+1)}>Increment</button>
+        {/* {count}
+        <button onClick={()=>setCount(count+1)}>Increment</button> */
+        
+        }
+        <h2>{timer}</h2>
         </>
      );
 }
